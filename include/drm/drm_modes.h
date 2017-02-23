c308f2fb792e6d12dab67a7730e5dd768e10bc43
Christoph Niedermaier <cniedermaier@dh-electronics.de>
2017-02-23 11:23:18 +0100

    drm: mode: Add function show_fb_videomode()

--- include/drm/drm_modes.h
+++ include/drm/drm_modes.h
@@ -205,6 +205,7 @@ void drm_display_mode_to_videomode(const struct drm_display_mode *dmode,
 void drm_display_mode_to_fb_videomode(const char *name,
 				      const struct drm_display_mode *dmode,
 				      struct fb_videomode *fbvm);
+void show_fb_videomode(const struct fb_videomode *fbvm);
 int bootargs_get_drm_display_mode(char **timings, int size,
 				  struct drm_display_mode *dmode);
 int of_get_drm_display_mode(struct device_node *np,
